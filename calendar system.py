06f8086f8e46d3026618e81da7d1e1525740de1a
XyKalus <xykalus@gmail.com>
2026-08-18 18:27:24 +0800

    got a small calendar system running, will need to make this print out automatically instead of asking the user everytime

--- calendar system.py
+++ calendar system.py
@@ -1,1 +1,18 @@
 import calendar
+
+# txtcal = calendar.TextCalendar(firstweekday=0)
+# print(txtcal)
+
+# days = list(calendar.day_name)
+# print(days)
+
+# calmonth = calendar.month_name
+# print(calmonth)
+
+# htmlcal = calendar.HTMLCalendar
+# print(htmlcal)
+
+year = int(input('enter the year: '))
+month = int(input('enter the month: '))
+
+print(calendar.month(year,month) )
