466350f4bff8b956a6aadfd1a0d3281b14775b01
XyKalus <xykalus@gmail.com>
2026-09-08 21:01:03 +0800

    started on the UI, i am so far behind

--- EventSystem.py
+++ EventSystem.py
@@ -1,6 +1,25 @@
 import datetime as dt
 from datetime import timedelta
 import calendar
+import sys
+import PyQt5
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Set an event date')
+        self.setGeometry(600,250,750,500) # (x,y,width,height)
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())       
+
+if __name__ == "__main__":
+    main()
+
 
 def eventsystem():
     today = dt.datetime.today() 
@@ -35,4 +54,3 @@ def eventsystem():
 
     print(calc)
 
-eventsystem()
