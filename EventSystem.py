5b747a2fe1cc4e323837cb45110c9451b68b4014
XyKalus <xykalus@gmail.com>
2026-09-09 16:40:36 +0800

    UI test (again)

--- EventSystem.py
+++ EventSystem.py
@@ -65,7 +65,7 @@ def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    sys.exit(app.exec_())       
+    sys.exit(app.exec())       
 
 if __name__ == "__main__":
     main()
